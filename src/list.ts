a24b53d13950ab9cee753e406a3073fdf9b7af02
Baptiste Pillot <baptiste@pillot.fr>
2025-03-10 17:38:22 +0100

    Dynamic actions, template target

--- src/list.ts
+++ src/list.ts
@@ -1,8 +1,11 @@
-import { Action }     from '@itrocks/action'
-import { Request }    from '@itrocks/action-request'
-import { Need }       from '@itrocks/action'
-import { Route }      from '@itrocks/route'
-import { dataSource } from '@itrocks/storage'
+import { Action }      from '@itrocks/action'
+import { ActionEntry } from '@itrocks/action'
+import { getActions }  from '@itrocks/action'
+import { Request }     from '@itrocks/action-request'
+import { Need }        from '@itrocks/action'
+import { Route }       from '@itrocks/route'
+import { routeOf }     from '@itrocks/route'
+import { dataSource }  from '@itrocks/storage'
 
 @Need('Store', 'new')
 @Route('/list')
@@ -13,7 +16,20 @@ export class List extends Action
 	{
 		const type    = request.type
 		const objects = await dataSource().search(type)
-		return this.htmlTemplateResponse({ objects, type }, request, __dirname + '/list.html')
+
+		const generalActions:   ActionEntry[] = []
+		const selectionActions: ActionEntry[] = []
+		const route = routeOf(this)
+		for (const action of getActions(type, route.slice(route.lastIndexOf('/') + 1))) {
+			const actions = (action.need === 'object') ? selectionActions : generalActions
+			actions.push(action)
+		}
+
+		return this.htmlTemplateResponse(
+			{ generalActions, objects, selectionActions, type },
+			request,
+			__dirname + '/list.html'
+		)
 	}
 
 	async json(request: Request)
