af686b6b3f3c83a025de3aed51f55fdb1f392a53
Baptiste Pillot <baptiste@pillot.fr>
2025-12-12 20:32:23 +0100

    Use TableLink

--- esm/build.ts
+++ esm/build.ts
@@ -3,36 +3,32 @@ import { TableFeed }                    from '../../table/feed.js'
 import { TableFreeze }                  from '../../table/freeze.js'
 import { TableFreezeInheritBackground } from '../../table/freeze/inherit-background.js'
 import { TableFreezeInheritBorder }     from '../../table/freeze/inherit-border.js'
+import { TableLink }                    from '../../table/link.js'
 import { tableByElement }               from '../../table/table.js'
 import { xTargetCall }                  from '../../xtarget/xtarget.js'
+
+const tableLink = new TableLink({
+	call: url => xTargetCall(url, 'main'),
+	href: 'action',
+	id:   (element: Element) => {
+		const input = element.closest('tr')?.querySelector<HTMLInputElement>(':scope > th.select > input[name=id]')
+		if (input) return { element: input as Element, value: input.value }
+	}
+})
 
 build<HTMLTableElement>(
 	'article[data-action="list"] > form > table.objects',
 	element => {
 		tableByElement(element, {
-			plugins: [ TableFeed, TableFreeze, TableFreezeInheritBackground, TableFreezeInheritBorder ]
+			plugins: [TableFeed, TableFreeze, TableFreezeInheritBackground, TableFreezeInheritBorder, tableLink]
 		})
-		element.querySelector(':scope > tbody')?.addEventListener('click', async event =>
-		{
-			const td = event.target
-			if (!(td instanceof HTMLTableCellElement)) return
-			const tr = td.closest('tr')
-			if (!tr) return
-			let select = td.closest('.select')
-			if (select) {
-				const input = select.querySelector('input')
-				if (!input) return
-				input.click()
-				input.focus()
-				return
-			}
-			const checkBox = tr.querySelector<HTMLInputElement>(':scope > th.select > input[name=id]')
-			if (!checkBox) return
-			const id = checkBox.value
-			if (!id) return
-			const form = tr.closest('form')
-			if (!form) return
-			return xTargetCall(form.getAttribute('action') + '/' + id, 'main')
+		element.addEventListener('click', event => {
+			const element = event.target
+			if (!(element instanceof HTMLTableCellElement) || !element.classList.contains('select')) return
+			const input = element.querySelector<HTMLInputElement>('input[type=checkbox]')
+			if (!input) return
+			input.click()
+			input.focus()
 		})
 	}
 )
